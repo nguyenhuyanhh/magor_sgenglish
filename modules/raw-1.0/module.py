917fa16ccf7341eb4d8335d185e732682f67d7bb
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-02-05 20:07:07 +0800

    Implement video (mp4) options for raw and resample

--- modules/raw-1.0/module.py
+++ modules/raw-1.0/module.py
@@ -20,6 +20,7 @@ ROOT_DIR = os.path.dirname(os.path.dirname(CUR_DIR))
 DATA_DIR = os.path.join(ROOT_DIR, 'data/')
 CRAWL_DIR = os.path.join(ROOT_DIR, 'crawl/')
 AUDIO_EXTS = ['.wav', '.mp3']
+VIDEO_EXTS = ['.mp4']
 
 MODULE_NAME = 'raw'
 LOG_H = logging.StreamHandler()
@@ -36,7 +37,7 @@ def raw(filename):
     """Import a file inside /crawl into /data."""
     file_path = os.path.join(CRAWL_DIR, filename)
     name, ext = os.path.splitext(filename)
-    if ext in AUDIO_EXTS:
+    if ext in AUDIO_EXTS or VIDEO_EXTS:
         file_id = slugify(name)
         working_dir = os.path.join(DATA_DIR, file_id + '/')
         raw_dir = os.path.join(working_dir, 'raw/')
--- modules/resample-1.0/module.py
+++ modules/resample-1.0/module.py
@@ -13,10 +13,13 @@ import os
 import sys
 
 from sox import Transformer
+from ffmpy import FFmpeg
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 ROOT_DIR = os.path.dirname(os.path.dirname(CUR_DIR))
 DATA_DIR = os.path.join(ROOT_DIR, 'data/')
+AUDIO_EXTS = ['.wav', '.mp3']
+VIDEO_EXTS = ['.mp4']
 
 logging.getLogger().disabled = True  # disable logging for sox
 
@@ -39,17 +42,28 @@ def resample(file_id):
     resample_dir = os.path.join(working_dir, 'resample/')
 
     # resample
-    audio_in = os.path.join(raw_dir, os.listdir(raw_dir)[0])
+    file_in = os.path.join(raw_dir, os.listdir(raw_dir)[0])
     audio_out = os.path.join(resample_dir, '{}.wav'.format(file_id))
     if os.path.exists(audio_out) and os.path.getsize(audio_out) > 0:
         LOG.debug('Previously resampled %s', audio_out)
     else:
         if not os.path.exists(resample_dir):
             os.makedirs(resample_dir)
-        tfm = Transformer()
-        tfm.convert(samplerate=16000, n_channels=1, bitdepth=16)
-        tfm.build(audio_in, audio_out)
-        LOG.debug('Resampled %s to %s', audio_in, audio_out)
+        if os.path.splitext(file_in)[1] in AUDIO_EXTS:
+            LOG.debug('Audio file, resampling using sox')
+            tfm = Transformer()
+            tfm.convert(samplerate=16000, n_channels=1, bitdepth=16)
+            tfm.build(file_in, audio_out)
+            LOG.debug('Resampled %s to %s', file_in, audio_out)
+        elif os.path.splitext(file_in)[1] in VIDEO_EXTS:
+            LOG.debug('Video file, resampling using ffmpeg')
+            inputs = {file_in: None}
+            outputs = {audio_out: '-ac 1 -ar 16000 -sample_fmt s16'}
+            ffmp = FFmpeg(inputs=inputs, outputs=outputs)
+            LOG.debug('Command: %s', ffmp.cmd)
+            fnull = open(os.devnull, 'w')
+            ffmp.run(stdout=fnull, stderr=fnull)  # silent output
+            LOG.debug('Resampled %s to %s', file_in, audio_out)
 
 if __name__ == '__main__':
     resample(sys.argv[1])
