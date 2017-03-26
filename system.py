a9852dcb5b492f28b915deedfc3bc4f83289763b
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-03-27 06:54:30 +0800

    Implement type-agnostic module call, integrate raw into core system

--- system.py
+++ system.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import logging
 import os
+import shutil
 import subprocess
 
 from slugify import slugify
@@ -26,6 +27,9 @@ with open(MANIFEST_FILE, 'r') as file_:
     MANIFEST = json.load(file_)
 PROCEDURES = MANIFEST['procedures']
 MODULES = MANIFEST['modules']
+FILE_TYPES = MANIFEST['file_types']
+VALID_TYPES = [x.decode('utf-8')
+               for x in FILE_TYPES['audio'] + FILE_TYPES['video']]
 
 
 def manifest_check():
@@ -58,7 +62,7 @@ def manifest_check():
     # check procedures
     proc_violate = list()
     for proc_id, proc_ in PROCEDURES.items():
-        proc_inputs = set()
+        proc_inputs = set(['raw'])
         for proc in proc_:
             if proc not in MODULES.keys():
                 LOG.info('Invalid module %s for procedure %s', proc, proc_id)
@@ -86,6 +90,8 @@ class Process():
     """
 
     def __init__(self, filename, procedure_id):
+        LOG.info('')
+        LOG.info('')
         self.filename = filename
         LOG.info('Filename: %s.', self.filename)
         self.file_id = slugify(os.path.splitext(filename)[0])
@@ -94,110 +100,49 @@ class Process():
         LOG.info('Procedure: %s.', self.procedure_id)
 
         # set later during verify()
-        self.raw_mod = None
-        self.resample_mod = None
-        self.diarize_mod = None
-        self.transcribe_mod = None
+        self.module_list = None
 
     def verify(self):
-        """Verify the procedure and modules."""
-        LOG.info('Verifying procedures and modules...')
-
-        # verify procedure
+        """Verify the file type and procedure."""
+        if (os.path.splitext(self.filename)[1]) not in VALID_TYPES:
+            LOG.info('%s is of invalid type', self.filename)
+            return False
         if self.procedure_id not in PROCEDURES.keys():
             LOG.info('Procedure %s does not exist', self.procedure_id)
             return False
-
-        # verify modules
-        procedure = PROCEDURES[self.procedure_id]
-        for type_, mod_ in procedure.items():
-            if mod_ not in MODULES.keys():
-                LOG.info("Module %s does not exist", mod_)
-                return False
-            module = MODULES[mod_]
-            if not module['type'] == type_:
-                LOG.info("Type %s and module %s do not match", type_, mod_)
-                return False
-            execs = module['requires'] + ['module.py']
-            for exec_ in execs:
-                exec_path = os.path.join(MODULES_DIR, mod_, exec_)
-                if not os.path.exists(exec_path):
-                    LOG.info('Module %s version %s: %s does not exist',
-                             mod_, module['version'], exec_)
-                    return False
-        LOG.info("Verification completed. Setting modules...")
-
-        # set modules
-        if 'raw' in procedure.keys():
-            self.raw_mod = procedure['raw']
-            LOG.info('Raw module: %s', self.raw_mod)
-        if 'resample' in procedure.keys():
-            self.resample_mod = procedure['resample']
-            LOG.info('Resample module: %s', self.resample_mod)
-        if 'diarize' in procedure.keys():
-            self.diarize_mod = procedure['diarize']
-            LOG.info('Diarize module: %s', self.diarize_mod)
-        if 'transcribe' in procedure.keys():
-            self.transcribe_mod = procedure['transcribe']
-            LOG.info('Transcribe module: %s', self.transcribe_mod)
-
+        # set module list
+        self.module_list = PROCEDURES[self.procedure_id]
         return True
 
-    def raw(self):
-        """Import a raw file."""
-        try:
-            exec_ = os.path.join(MODULES_DIR, self.raw_mod, 'module.py')
-            args = ['python', exec_, self.filename]
-            subprocess.call(args)
-            LOG.info('Import completed for %s using module %s',
-                     self.file_id, self.raw_mod)
-        except:
-            LOG.info('Error occured.', exc_info=True)
+    def import_file(self):
+        """Import the file into /data."""
+        file_path = os.path.join(CRAWL_DIR, self.filename)
+        working_dir = os.path.join(DATA_DIR, self.file_id)
+        raw_dir = os.path.join(working_dir, 'raw/')
+        raw_file = os.path.join(raw_dir, self.filename)
+        if os.path.exists(raw_file):
+            LOG.info('Previously imported to %s', raw_file)
+        else:
+            if not os.path.exists(raw_dir):
+                os.makedirs(raw_dir)
+            shutil.copy2(file_path, raw_dir)
+            LOG.info('Imported %s to %s', file_path, raw_dir)
 
-    def resample(self):
-        """Resample a file using module resample."""
+    def call(self, module_id):
+        """Atom instruction to call a module."""
         try:
-            exec_ = os.path.join(MODULES_DIR, self.resample_mod, 'module.py')
+            exec_ = os.path.join(MODULES_DIR, module_id, 'module.py')
             args = ['python', exec_, self.file_id]
             subprocess.call(args)
-            LOG.info('Resampling completed for %s using module %s',
-                     self.file_id, self.resample_mod)
-        except:
-            LOG.info('Error occured.', exc_info=True)
-
-    def diarize(self):
-        """Diarize a file using module diarize."""
-        try:
-            exec_ = os.path.join(MODULES_DIR, self.diarize_mod, 'module.py')
-            args = ['python', exec_, self.file_id]
-            subprocess.call(args)
-            LOG.info('Diarization completed for %s using module %s',
-                     self.file_id, self.diarize_mod)
-        except:
-            LOG.info('Error occured.', exc_info=True)
-
-    def transcribe(self):
-        """Transcribe a file using module google or lvcsr."""
-        try:
-            exec_ = os.path.join(MODULES_DIR, self.transcribe_mod, 'module.py')
-            args = ['python', exec_, self.file_id]
-            subprocess.call(args)
-            LOG.info('Transcription completed for %s using module %s',
-                     self.file_id, self.transcribe_mod)
         except:
             LOG.info('Error occured.', exc_info=True)
 
     def pipeline(self):
         """Pipeline for processing."""
         if self.verify():
-            if self.raw_mod is not None:
-                self.raw()
-            if self.resample_mod is not None:
-                self.resample()
-            if self.diarize_mod is not None:
-                self.diarize()
-            if self.transcribe_mod is not None:
-                self.transcribe()
+            self.import_file()
+            for mod_id in self.module_list:
+                self.call(mod_id)
             LOG.info('Pipeline completed for %s using procedure %s',
                      self.filename, self.procedure_id)
         else:
@@ -207,6 +152,7 @@ class Process():
 
 def workflow(procedure_list):
     """Processing workflow, using a procedure list."""
+    manifest_check()
     for filename in os.listdir(CRAWL_DIR):
         path_ = os.path.join(CRAWL_DIR, filename)
         if os.path.isfile(path_):
