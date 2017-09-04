a159edf1e0e041543b25e00216b4172709269b45
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-09-04 15:55:54 +0800

    visualize: multiple transcripts

--- modules/visualize-1.0/module.py
+++ modules/visualize-1.0/module.py
@@ -8,6 +8,7 @@ Requires: image.png
 Visualize a file_id with transcripts into /visualize
 """
 
+import json
 import logging
 import os
 import shutil
@@ -42,38 +43,73 @@ def secs_to_timestamp(secs):
     return '{:02d}:{:02d}:{:02d},{:03d}'.format(time_h, time_m, time_s, time_ms)
 
 
-def tg_to_srt(textgrid_file, srt_file):
-    """Convert a TextGrid file to an SRT file."""
+def tg_to_srt(textgrid_file, id_, temp_file):
+    """Convert a TextGrid file to an SRT data structure."""
     srt = dict()
-    with open(textgrid_file, 'r') as file_:
-        lines = file_.readlines()
 
-    # convert textgrid to dict structure
-    i = 0
-    while i < len(lines):
-        if lines[i].startswith(' ' * 8 + 'name'):
-            # find the spk_id
-            spk_id = lines[i].strip().split()[2].strip('"')
-            # get the xmin, xmax and text
-            i += 5
-            while i < len(lines) and lines[i].startswith(' ' * 12 + 'xmin'):
-                xmin = lines[i].strip().split()[2]
-                xmin_key = int(Decimal(xmin) * 100)
-                xmax = lines[i + 1].strip().split()[2]
-                text = ' '.join(lines[i + 2].strip().split()[2:]).strip('"')
-                trans = '{}: {}'.format(spk_id, text)
-                srt[xmin_key] = [xmin, xmax, trans]
-                i += 4
-        i += 1
+    # complete checks
+    if os.path.exists(temp_file) and os.path.getsize(temp_file) > 0:
+        with open(temp_file, 'r') as json_:
+            tmp = json.load(json_)
+            srt = {int(k): v for k, v in tmp.items()}
+        LOG.debug('tg_to_srt operation previously completed for %s', textgrid_file)
+    else:
+        with open(textgrid_file, 'r') as file_:
+            lines = file_.readlines()
 
-    # convert dict structure to srt
+        # convert textgrid to dict structure
+        i = 0
+        while i < len(lines):
+            if lines[i].startswith(' ' * 8 + 'name'):
+                # find the spk_id
+                spk_id = lines[i].strip().split()[2].strip('"')
+                # get the xmin, xmax and text
+                i += 5
+                while i < len(lines) and lines[i].startswith(' ' * 12 + 'xmin'):
+                    xmin = lines[i].strip().split()[2]
+                    xmin_key = int(Decimal(xmin) * 100)
+                    xmax = lines[i + 1].strip().split()[2]
+                    text = ' '.join(
+                        lines[i + 2].strip().split()[2:]).strip('"')
+                    trans = '[{}]{}: {}'.format(id_, spk_id, text)
+                    srt[xmin_key] = {
+                        'xmin': xmin,
+                        'xmax': xmax,
+                        'trans': trans,
+                    }
+                    i += 4
+            i += 1
+
+        # write output
+        with open(temp_file, 'w') as json_:
+            json.dump(srt, json_, indent=4, sort_keys=True)
+        LOG.debug('tg_to_srt operation completed for %s', textgrid_file)
+
+    return srt
+
+
+def combine_srt(srt1, srt2):
+    """Combine two srt data structures together."""
+    result = dict()
+    for key, value in srt1.items():
+        result[key] = {
+            'xmin': value['xmin'],
+            'xmax': value['xmax'],
+            'trans': value['trans'] + '\n' + srt2[key]['trans']
+        }
+    LOG.debug('combine_srt operation completed')
+    return result
+
+
+def write_srt(srt, srt_file):
+    """Write srt file from srt data structure."""
     i = 1
     srt_dict = dict()
     for key in sorted(srt):
         srt_dict[i] = list()
         srt_dict[i].append(
-            '{} --> {}'.format(secs_to_timestamp(srt[key][0]), secs_to_timestamp(srt[key][1])))
-        srt_dict[i].append(srt[key][2])
+            '{} --> {}'.format(secs_to_timestamp(srt[key]['xmin']), secs_to_timestamp(srt[key]['xmax'])))
+        srt_dict[i].append(srt[key]['trans'])
         i += 1
 
     with open(srt_file, 'w') as file_:
@@ -108,16 +144,38 @@ def visualize(file_id):
     raw_file = os.path.join(raw_dir, os.listdir(raw_dir)[0])
     resample_dir = os.path.join(working_dir, 'resample/')
     resample_file = os.path.join(resample_dir, '{}.wav'.format(file_id))
-    trans_dir = os.path.join(working_dir, 'transcript/google')
-    textgrid_file = os.path.join(trans_dir, '{}.TextGrid'.format(file_id))
+    trans_dir = os.path.join(working_dir, 'transcript')
     visualize_dir = os.path.join(working_dir, 'visualize/')
     if not os.path.exists(visualize_dir):
         os.makedirs(visualize_dir)
     srt_file = os.path.join(visualize_dir, '{}.srt'.format(file_id))
     video_file = os.path.join(visualize_dir, '{}.mp4'.format(file_id))
+    temp_dir = os.path.join(working_dir, 'temp/visualize')
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
 
-    # operations
-    tg_to_srt(textgrid_file, srt_file)
+    # write srt
+    tmp = list()
+    if os.listdir(trans_dir):
+        # transform all textgrids to srt data structure
+        for trans in os.listdir(trans_dir):
+            trans_file = os.path.join(
+                trans_dir, trans, '{}.TextGrid'.format(file_id))
+            temp_file = os.path.join(temp_dir, '{}.json'.format(trans))
+            tmp.append(tg_to_srt(trans_file, trans, temp_file))
+
+        # merge srt data structures if necessary
+        srt_data = tmp[0]
+        if len(tmp) >= 2:
+            for i in range(1, len(tmp)):
+                srt_data = combine_srt(srt_data, tmp[i])
+
+        # write srt
+        write_srt(srt_data, srt_file)
+    else:
+        LOG.debug('No transcripts found for %s', file_id)
+
+    # produce video_file
     # complete checks for video_file
     if os.path.exists(video_file) and os.path.getsize(video_file) > 0:
         LOG.debug('Previously processed %s', video_file)
