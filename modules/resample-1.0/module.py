77f38803d405a120f7535c6abab4c5e9c9e8c113
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-09-13 09:35:29 +0800

    resample: multiple raw files

--- modules/resample-1.0/module.py
+++ modules/resample-1.0/module.py
@@ -29,28 +29,52 @@ LOG.addHandler(LOG_H)
 LOG.setLevel(logging.DEBUG)
 
 
-def resample(file_id):
-    """Resample a file_id."""
-    # init paths
-    working_dir = os.path.join(DATA_DIR, file_id)
-    raw_dir = os.path.join(working_dir, 'raw/')
-    resample_dir = os.path.join(working_dir, 'resample/')
+def resample_audio(audio_in, audio_out):
+    """Resample an audio stream.
 
-    # resample
-    audio_in = os.path.join(raw_dir, os.listdir(raw_dir)[0])
-    audio_out = os.path.join(resample_dir, '{}.wav'.format(file_id))
+    Arguments:
+        audio_in: str - path to input stream
+        audio_out: str - path to output stream
+    """
+    # complete checks
     if os.path.exists(audio_out) and os.path.getsize(audio_out) > 0:
         LOG.debug('Previously resampled %s', audio_out)
     else:
-        if not os.path.exists(resample_dir):
-            os.makedirs(resample_dir)
         inputs = {audio_in: None}
         outputs = {audio_out: '-ac 1 -ar 16000 -sample_fmt s16'}
         ffmp = FFmpeg(inputs=inputs, outputs=outputs)
-        LOG.debug('Resampling command: %s', ffmp.cmd)
         fnull = open(os.devnull, 'w')
         ffmp.run(stdout=fnull, stderr=fnull)  # silent output
         LOG.debug('Resampled %s to %s', audio_in, audio_out)
+
+
+def resample(file_id):
+    """Resample a file_id.
+
+    Arguments:
+        file_id: str - file id
+    """
+    # init paths
+    working_dir = os.path.join(DATA_DIR, file_id)
+    raw_dir = os.path.join(working_dir, 'raw/')
+    raw_files = os.listdir(raw_dir)
+    resample_dir = os.path.join(working_dir, 'resample/')
+    if not os.path.exists(resample_dir):
+        os.makedirs(resample_dir)
+
+    # case 1: single file
+    if len(raw_files) == 1:
+        audio_in = os.path.join(raw_dir, os.listdir(raw_dir)[0])
+        audio_out = os.path.join(resample_dir, '{}.wav'.format(file_id))
+        resample_audio(audio_in, audio_out)
+    # case 2: more than one file
+    elif raw_files:
+        for file_ in raw_files:
+            audio_in = os.path.join(raw_dir, file_)
+            audio_out = os.path.join(
+                resample_dir, '{}.wav'.format(os.path.splitext(file_)[0]))
+            resample_audio(audio_in, audio_out)
+
 
 if __name__ == '__main__':
     resample(sys.argv[1])
